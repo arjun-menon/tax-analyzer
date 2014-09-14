586bb9ee1c4c889563f5a212cca2bcedef588db4
Arjun G. Menon <contact@arjungmenon.com>
2014-09-14 16:53:57 +0400

    Split nyc_tax_calc.js -> nyc_taxes.js (core) & calc_nyc_taxes.js (CLI)

---
+++ calc_nyc_taxes.js
@@ -0,0 +1,44 @@
+
+nyc_taxes = require('./nyc_taxes.js')
+
+function node_console_main() {
+    console_help_message = "\
+usage: node calc_nyc_taxes.js [-h] income [-d DEDUCTIONS] [-e EXEMPTIONS]\n\
+\n\
+Calculates total U.S. taxes owed by a single (unmarried) resident of NYC.\n\
+\n\
+positional arguments:\n\
+  income                Annual Gross Income\n\
+\n\
+optional arguments:\n\
+  -h, --help            show this help message and exit\n\
+  -d DEDUCTIONS, --deductions DEDUCTIONS\n\
+                        Itemized deduction other than the standard deduction,\n\
+                        personal exemptions, and state/local/foreign tax\n\
+                        deductions.\n\
+  -e EXEMPTIONS, --exemptions EXEMPTIONS\n\
+                        Number of personal exemptions. The default is 1."
+
+    if ( process.argv.length < 3 || process.argv[2] == "-h" ) {
+        console.log(console_help_message)
+    }
+    else {
+        income = parseInt( process.argv[2] )
+
+        deductions = 0
+        exemptions = 1
+
+        for(i = 3; i < (process.argv.length - 1); i += 2) {
+            if ( process.argv[i] == '-d' )
+                deductions = parseInt( process.argv[i+1] )
+            if ( process.argv[i] == '-e' )
+                exemptions = parseInt( process.argv[i+1] )
+        }
+
+        nyc_taxes.calc_taxes(income, deductions, exemptions)
+    }
+}
+
+if (require.main === module) {
+    node_console_main()
+}
